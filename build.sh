0722a16391dc450bda41ec59be65951f35b54666
Raymond Velasquez <ray@monvelasquez.com>
2026-05-04 07:02:22 +0800

    chore: clearnup build script

--- build.sh
+++ build.sh
@@ -23,23 +23,11 @@ docker buildx build --platform linux/amd64,linux/arm64 \
 
 docker tag ghcr.io/$GITHUB_USER/dockcheck-web:latest ghcr.io/$GITHUB_USER/dockcheck-web:$1.$2.$3
 
-echo "* logging in to github"
+
+# echo "* logging in to github"
 echo $CR_PAT | docker login ghcr.io -u $GITHUB_USER --password-stdin
 
-# docker push ghcr.io/$GITHUB_USER/dockcheck-web:$1.$2.$3
-# docker push ghcr.io/$GITHUB_USER/dockcheck-web:latest
-
-
-
-# docker build --platform linux/arm64/v8 \
-#     -t ghcr.io/$GITHUB_USER/dockcheck-web:latest-arm64v8 \
-#     --build-arg ARCH=linux/arm64 \
-#     --build-arg DOCKCHECK_VERSION=0.5.8.0 \
-#     --build-arg DCW_VERSION=$1.$2.$3 .
-
-# docker build --platform linux/amd64 \
-#     -t ghcr.io/$GITHUB_USER/dockcheck-web:latest-amd64   \
-#     --build-arg ARCH=linux/amd64 \
-#     --build-arg DOCKCHECK_VERSION=0.5.8.0 \
-#     --build-arg DCW_VERSION=$1.$2.$3 .
+# push to  ghcr.io
+docker push ghcr.io/$GITHUB_USER/dockcheck-web:$1.$2.$3
+docker push ghcr.io/$GITHUB_USER/dockcheck-web:latest
 
